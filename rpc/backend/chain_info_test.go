c2146d4a42e94712154d466a4369f0165858483d
Ivan Shukhov <45999338+ivanshukhov@users.noreply.github.com>
2023-11-07 12:21:41 +0100

    fix(rpc): align fee history (#1611)
    * update nix
    
    * add next fee in fee history
    
    * fix test
    
    * add change doc
    
    * height + 1 for next fee
    
    * cross check baseFeePerGas len
    
    * Update tests/integration_tests/test_fee_history.py
    
    Co-authored-by: MalteHerrmann <42640438+MalteHerrmann@users.noreply.github.com>
    
    * fix oldestBlock & align earliest input as eth
    
    * update doc
    
    * update nix
    
    * isort test_fee_history.py
    
    * fix test
    
    * align rpc res as eth
    
    * add cross check
    
    * add baseFeePerGas len check
    
    * add oldestBlock check
    
    Co-authored-by: MalteHerrmann <42640438+MalteHerrmann@users.noreply.github.com>
    Co-authored-by: Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
    (cherry picked from commit 47fdfd3d8318358c2578b610bc35bc8c1ebb131e)
    
    # Conflicts:
    #	CHANGELOG.md
    #	gomod2nix.toml
    #	rpc/backend/chain_info.go

--- rpc/backend/chain_info_test.go
+++ rpc/backend/chain_info_test.go
@@ -338,7 +338,7 @@ func (suite *BackendTestSuite) TestFeeHistory() {
 				RegisterParamsError(queryClient, &header, ethrpc.BlockNumber(1).Int64())
 			},
 			1,
-			0,
+			-1,
 			nil,
 			nil,
 			false,
@@ -352,7 +352,7 @@ func (suite *BackendTestSuite) TestFeeHistory() {
 				RegisterParams(queryClient, &header, ethrpc.BlockNumber(1).Int64())
 			},
 			1,
-			0,
+			-1,
 			nil,
 			nil,
 			false,
@@ -406,6 +406,7 @@ func (suite *BackendTestSuite) TestFeeHistory() {
 		{
 			"pass - Valid FeeHistoryResults object",
 			func(validator sdk.AccAddress) {
+				var header metadata.MD
 				baseFee := sdk.NewInt(1)
 				queryClient := suite.backend.queryClient.QueryClient.(*mocks.EVMQueryClient)
 				client := suite.backend.clientCtx.Client.(*mocks.Client)
@@ -415,12 +416,14 @@ func (suite *BackendTestSuite) TestFeeHistory() {
 				RegisterBaseFee(queryClient, baseFee)
 				RegisterValidatorAccount(queryClient, validator)
 				RegisterConsensusParams(client, 1)
+				RegisterParams(queryClient, &header, 1)
+				RegisterParamsWithoutHeader(queryClient, 1)
 			},
 			1,
 			1,
 			&rpc.FeeHistoryResult{
-				OldestBlock:  (*hexutil.Big)(big.NewInt(0)),
-				BaseFee:      []*hexutil.Big{(*hexutil.Big)(big.NewInt(1))},
+				OldestBlock:  (*hexutil.Big)(big.NewInt(1)),
+				BaseFee:      []*hexutil.Big{(*hexutil.Big)(big.NewInt(1)), (*hexutil.Big)(big.NewInt(1))},
 				GasUsedRatio: []float64{0},
 				Reward:       [][]*hexutil.Big{{(*hexutil.Big)(big.NewInt(0)), (*hexutil.Big)(big.NewInt(0)), (*hexutil.Big)(big.NewInt(0)), (*hexutil.Big)(big.NewInt(0))}},
 			},
